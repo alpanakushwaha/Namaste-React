73d4128e41845e55c4151541a396d5efae186e09
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-15 13:25:58 +0530

    Chap01_Inception HelloWorld using React

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello World!!!";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const root = document.getElementById("root");
+const list1 = React.createElement(
+  "li",
+  {
+    id: "listitem1",
+    className:"listClass",
+    style: {
+      fontSize: "25px",
+    },
+  },
+  "List Item One"
+);
+const list2 = React.createElement("li", { id: "listitem2" }, "List Item Two");
 
-root.appendChild(heading);
+const unOrderdList = React.createElement(
+  "ul",
+  {
+    id: "unorderedlist",
+    style: {
+      listStyle: "none",
+      color: "#00bb00",
+    },
+  },
+  list1,
+  list2
+);
+
+const heading = React.createElement(
+  "h1",
+  {
+    id: "heading1",
+    style: {
+      margin: "20px",
+      color: "#0000ff",
+    },
+  },
+  "Hello World! from Namaste React! 🙏",
+  unOrderdList
+);
+
+root.render(heading);
