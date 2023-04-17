c3861f5ef903f999474eb06b4235804375e18fb1
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-17 16:19:22 +0530

    Chap_03_LayingTheFoundation Nesting Using JSX

--- script.js
+++ script.js
@@ -1,56 +1,14 @@
-/*
-PARCEL:
-Created a server
-HMR -Hot module Replacement
-File Watcher algorithm-C++
-Bundling
-Differential Bundling=> supports older browser 
-MINIFY
-Cleaning our Code
-Dev and Production Build
-Different development & production bundles
-Super Fast build algorithm
-Image Optimization
-Caching while development
-Compression
-Compatible with older version of browsers
-HTTPS on dev
-Handels multiple port numbers
-Consistent Hashing Algorithms
-Zero config
-Error Handling
-Tree Shaking -removes unused code
-*/
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const h1 = React.createElement(
-  "h1",
-  { key: "heading1" },
-  "This h1 is a ReactElement."
-);
-
-const h2 = React.createElement(
-  "h2",
-  { key: "heading2" },
-  "This h2 is a ReactElement."
-);
-
-const h3 = React.createElement(
-  "h3",
-  { key: "heading3" },
-  "This h3 is a ReactElement."
-);
-
-const div = React.createElement(
-  "div",
-  { className: "title", key: "division" },
-  h1,
-  h2,
-  h3
+const div = (
+  <div className="title">
+    <h1>This h1 is reactElement written in JSX.</h1>
+    <h2>This h2 is reactElement written in JSX.</h2>
+    <h3>This h3 is reactElement written in JSX.</h3>
+  </div>
 );
 
 root.render(div);
