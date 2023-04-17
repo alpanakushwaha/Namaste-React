2478c5756c10155266c42a0254e630468578bd0e
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-17 16:38:10 +0530

    Chap_03_LayingTheFoundation FunctionalComponent Using JSX

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const div = (
-  <div className="title">
-    <h1>This h1 is reactElement written in JSX.</h1>
-    <h2>This h2 is reactElement written in JSX.</h2>
-    <h3>This h3 is reactElement written in JSX.</h3>
-  </div>
+const h2 = (
+  <h2 className="headerTwo" key="keyTwo">
+    h2 inside functional component named DividerComponent.
+  </h2>
 );
 
-root.render(div);
+const DividerComponent = () => {
+  return (
+    <div className="title">
+      <h1 className="headerOne" key="keyOne">
+        h1 inside functional component named DividerComponent.
+      </h1>
+      {h2}
+      <h3 className="headerThree" key="keyThree">
+        h3 inside functional component named DividerComponent.
+      </h3>
+    </div>
+  );
+};
+
+root.render(<DividerComponent />);
