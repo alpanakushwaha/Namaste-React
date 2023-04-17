756ee7820c4ab1445a3177b5c43b32518eb1eaac
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-17 18:20:18 +0530

    Chap_03_LayingTheFoundation, component inside component

--- script.js
+++ script.js
@@ -3,13 +3,15 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const TitleComponent = <h1> Interesting Title.</h1>;
+const TitleComponent = () => {
+  return <h1> Interesting Title.</h1>;
+};
 
 const HeaderComponent = () => {
   return (
     <div>
-      {TitleComponent}
-           
+      {<TitleComponent />}
+      {<TitleComponent></TitleComponent>}
     </div>
   );
 };
