5bf1a529901130858ead86bf3b896f7dd8c998ce
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-17 18:23:19 +0530

    Chap_03_LayingTheFoundation, clear slate

--- script.js
+++ script.js
@@ -3,18 +3,3 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const TitleComponent = () => {
-  return <h1> Interesting Title.</h1>;
-};
-
-const HeaderComponent = () => {
-  return (
-    <div>
-      {<TitleComponent />}
-      {<TitleComponent></TitleComponent>}
-    </div>
-  );
-};
-
-// root.render(HeaderComponent());
-root.render(<HeaderComponent />);
