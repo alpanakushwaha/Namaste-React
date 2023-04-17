95ff8c4400c1098c0f731a35b2caa5b5c39a85d1
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-17 18:01:54 +0530

    Chap_03_LayingTheFoundation, element inside component

--- script.js
+++ script.js
@@ -3,3 +3,16 @@ import ReactDOM from "react-dom/client";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const TitleComponent = <h1> Interesting Title.</h1>;
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      {TitleComponent}
+           
+    </div>
+  );
+};
+
+// root.render(HeaderComponent());
+root.render(<HeaderComponent />);
