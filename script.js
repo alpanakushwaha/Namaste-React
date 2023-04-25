1b801ff6fcf069302cb2c924c3c6ba20e1fa646b
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-26 00:33:28 +0530

    Chap_04 Dynamic Cards cuisine.join() added in RestaurantCard)

--- script.js
+++ script.js
@@ -658,7 +658,7 @@ const RestaurantCard = ({
       />
       <div className="short-desc">
         <h4>{name}</h4>
-        <h5>{cuisines}</h5>
+        <h5>{cuisines.join(", ")}</h5>
         <h5>₹{costForTwo / 100} for two</h5>
         <h6>{avgRating} *</h6>
         <h6>at your doorstep within {deliveryTime} minutes</h6>
