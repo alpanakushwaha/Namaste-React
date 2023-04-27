1a0a809aa8591cbf580fe51a5dd77b34930c8088
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-27 21:05:32 +0530

    Chap_05, cleaning the code by having components, config folder in src, default export & import

---
+++ src/components/Header.js
@@ -0,0 +1,57 @@
+const TitleLogo = () => {
+  // <h1 id="title" key="h2">
+  //   Food Villa
+  // </h1>
+  return (
+    <a href="https://www.facebook.com/foodvillarestaurant1/photos/">
+      <img
+        className="logoImage"
+        src="https://lh3.googleusercontent.com/Em7AHf7XBH_RtGfCBVXz9RH8SM_pHkj3xPP-yd3cRguY1_Jc8fmqgx6WxnvGVyPV5xs5gL3HCD0FCuv6Xo4CwoY6ak4"
+        alt="logo"
+      />
+    </a>
+  );
+};
+
+const UserIcon = () => (
+  <img
+    className="user-icon"
+    src="https://static.vecteezy.com/system/resources/thumbnails/005/545/335/small/user-sign-icon-person-symbol-human-avatar-isolated-on-white-backogrund-vector.jpg "
+  ></img>
+);
+
+const CartIcon = () => (
+  <a href="#">
+    <img
+      className="cart-icon"
+      src="https://www.freepnglogos.com/uploads/shopping-cart-png/shopping-cart-svg-png-icon-download-28.png"
+      alt="cart logo"
+      width="50"
+      height="50"
+    />
+  </a>
+);
+
+const NavBar = () => (
+  <div className="navItems">
+    <ul>
+      <li>Home </li>
+      <li>About Us</li>
+      <li>Contact</li>
+    </ul>
+
+    <UserIcon />
+    <CartIcon />
+  </div>
+);
+
+const Header = () => {
+  return (
+    <div className="header">
+      <TitleLogo />
+      <NavBar />
+    </div>
+  );
+};
+
+export default Header;
