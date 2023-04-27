36f5ab2fb188834d8837b1dbc09a4188874966d2
Alpana Kushwaha <alpana95kush@gmail.com>
2023-04-27 23:19:07 +0530

    Chap_05, searchText without useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,4 +1,4 @@
-import { restaurantListObj } from "../config/mockData";
+import restaurantListObj from "../config/mockData";
 import { CDN_IMG_URL } from "../config/constants";
 const RestaurantCard = ({
   cloudinaryImageId,
@@ -11,12 +11,7 @@ const RestaurantCard = ({
 }) => {
   return (
     <div className="res-card">
-      <img
-        src={CDN_IMG_URL
-           +
-          cloudinaryImageId
-        }
-      />
+      <img src={CDN_IMG_URL + cloudinaryImageId} />
       <div className="short-desc">
         <h4>{name}</h4>
         <h5>{cuisines.join(", ")}</h5>
@@ -30,18 +25,36 @@ const RestaurantCard = ({
 };
 
 const Body = () => {
+  const searchText = "PizzaHut"; // hard-coded
   return (
     <div className="body">
-      <div className="RestaurantList">
-        {restaurantListObj.map((restaurantListObj) => {
-          return (
-            <RestaurantCard
-              {...restaurantListObj.data}
-              key={restaurantListObj.data.id}
-            />
-          );
-        })}
-      </div>
+      <>
+        <div className="search-container">
+          <input
+            type="text"
+            className="search-input"
+            placeholder="Search"
+            value={searchText} // one-way data-Binding
+            // onChange={(e)=> console.log("to show that you are trying to change the input, but can't.")}
+
+            onChange={(e) => {
+              searchText = e.target.value;
+            }}
+            // on-console, you'll see the character being pressed appended with the value
+          ></input>
+          <button className="search-btn">Search</button>
+        </div>
+        <div className="RestaurantList">
+          {restaurantListObj.map((restaurantListObj) => {
+            return (
+              <RestaurantCard
+                {...restaurantListObj.data}
+                key={restaurantListObj.data.id}
+              />
+            );
+          })}
+        </div>
+      </>
     </div>
   );
 };
