5abc1b3ab3cd375e306d69e8e38285a2e8273d7b
Alpana Kushwaha <alpana95kush@gmail.com>
2023-05-05 13:27:05 +0530

    Chap_07, useParams to get ID, 2nd way

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -2,10 +2,11 @@ import { useParams } from "react-router-dom";
 
 const RestaurantMenu = () => {
   const params = useParams();
+  const { id } = params;
   console.log(params);
   return (
     <div>
-      <h1>Restaurant id: andfdyID</h1>
+      <h1>Restaurant id: {id}</h1>
       <h3>Namaste Restaurant!!!</h3>
     </div>
   );
